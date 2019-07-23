8fff70d99b8c3cf0e0c9f8122e9ccb0133054ebe
Stefan <oyetorostephven@gmail.com>
2019-07-23 10:55:58 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,10 +2,7 @@ import calculator
 
 
 class TestCalculator:
-
-
-    def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+    
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
